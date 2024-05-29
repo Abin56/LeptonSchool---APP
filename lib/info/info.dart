de3e5019a743999256aef72016e5acfaf56fb9cf
Abin56 <abinjohn8089@gmail.com>
2024-05-29 17:21:34 +0530

    first commit

---
+++ lib/info/info.dart
@@ -0,0 +1,9 @@
+const  String  appLogo= "assets/excel_karror/excelKaroor.png";
+const String  officialLogo ="assets/excel_karror/excel_karoor_logo.png";
+const String nameOfInstitution = "EXCEL KAROOR";
+const String nameInSmall = "Karoor";
+const String nameInCapital = "EXCEL KAROOR";
+const String companyName ="EXCEL";
+const String companyNameSmall = "Excel";
+const String chat ='ExcelKaroor Chat';
+const String live ='ExcelKaroor Live';
